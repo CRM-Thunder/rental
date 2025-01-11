5936763003150b12b8190c8acfe6dc460c259d57
CRM-Thunder <damian-zamecki@wp.pl>
2025-01-11 21:51:18 +0100

    cors

--- wypozyczalnia/server.js
+++ wypozyczalnia/server.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+const cors=require('cors');
 const express = require("express");
 const bodyParser = require("body-parser");
 const carRoutes = require("./car_routes.js");
 const repository=require("./repository/repository.js");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/rental", carRoutes);
 
